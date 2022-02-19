0875f3ca2e858ac81630996c24c4592e3c63e729
SivkovAV <stereoworkmail@gmail.com>
2022-02-20 01:35:59 +0300

    update RadixHistoryWithExternReadCache using current version of RadixHistory

--- rspace/src/main/scala/coop/rchain/rspace/history/instances/RadixHistoryWithExternReadCache.scala
+++ rspace/src/main/scala/coop/rchain/rspace/history/instances/RadixHistoryWithExternReadCache.scala
@@ -4,8 +4,7 @@ import cats.Parallel
 import cats.effect.Sync
 import cats.syntax.all._
 import coop.rchain.rspace.hashing.Blake2b256Hash
-import coop.rchain.rspace.history.RadixTreeWithExternReadCache._
-import coop.rchain.rspace.history.LimitTrieMapCache
+import coop.rchain.rspace.history.RadixTree._
 import coop.rchain.rspace.history._
 import coop.rchain.shared.syntax.sharedSyntaxKeyValueStore
 import coop.rchain.store.{KeyValueStore, KeyValueTypedStore}
@@ -15,23 +14,24 @@ import scodec.bits.ByteVector
   * History implementation with radix tree
   */
 object RadixHistoryWithExternReadCache {
-  val emptyRootHash: Blake2b256Hash = Blake2b256Hash.fromByteArray(hashNode(emptyNode)._1.toArray)
+  val emptyRootHash: Blake2b256Hash = Blake2b256Hash.fromByteVector(hashNode(emptyNode)._1)
 
   /**
-    * Cache for storing read and decoded nodes.
-    * In cache load kv-pair (hash, node).
-    * Where hash  - Blake2b256Hash of serializing nodes data,
-    *       node - deserialized data of this node.
-    */
+   * Cache for storing read and decoded nodes.
+   * In cache load kv-pair (hash, node).
+   * Where hash  - Blake2b256Hash of serializing nodes data,
+   *       node - deserialized data of this node.
+   */
   val maxCacheRecordCount = 50
   val cache: LimitTrieMapCache[ByteVector, Node] =
     new LimitTrieMapCache[ByteVector, Node](maxCacheRecordCount)
+
   def apply[F[_]: Sync: Parallel](
       root: Blake2b256Hash,
       store: KeyValueTypedStore[F, ByteVector, ByteVector]
   ): F[RadixHistoryWithExternReadCache[F]] =
     for {
-      impl <- Sync[F].delay(new RadixTreeImpl[F](store, cache))
+      impl <- Sync[F].delay(new RadixTreeImpl[F](store))
       node <- impl.loadNode(root.bytes, noAssert = true)
     } yield RadixHistoryWithExternReadCache(root, node, impl, store, cache)
 
@@ -54,7 +54,7 @@ final case class RadixHistoryWithExternReadCache[F[_]: Sync: Parallel](
 
   override def reset(root: Blake2b256Hash): F[History[F]] =
     for {
-      impl <- Sync[F].delay(new RadixTreeImpl[F](store, cache))
+      impl <- Sync[F].delay(new RadixTreeImpl[F](store))
       node <- impl.loadNode(root.bytes, noAssert = true)
     } yield this.copy(root, node, impl, store)
 
@@ -63,7 +63,8 @@ final case class RadixHistoryWithExternReadCache[F[_]: Sync: Parallel](
 
   override def process(actions: List[HistoryAction]): F[History[F]] =
     for {
-      _ <- new RuntimeException("Cannot process duplicate actions on one key").raiseError
+      // TODO: To improve time perfomance, it is possible to implement this check into the makeActions().
+      _ <- new RuntimeException("Cannot process duplicate actions on one key.").raiseError
             .unlessA(hasNoDuplicates(actions))
 
       newRootNodeOpt <- impl.makeActions(rootNode, actions)
@@ -75,7 +76,7 @@ final case class RadixHistoryWithExternReadCache[F[_]: Sync: Parallel](
     } yield
       if (newRootHash.isDefined)
         this.copy(
-          Blake2b256Hash.fromByteArray(newRootHash.get.toArray),
+          Blake2b256Hash.fromByteVector(newRootHash.get),
           newRootNodeOpt.get,
           impl,
           store
@@ -83,5 +84,5 @@ final case class RadixHistoryWithExternReadCache[F[_]: Sync: Parallel](
       else this
 
   private def hasNoDuplicates(actions: List[HistoryAction]) =
-    actions.map(_.key).toSet.size == actions.size
+    actions.map(_.key).distinct.size == actions.size
 }
