c6f3fb48d6c32fc63ed7a4fc9eeb1cd6358cde00
SivkovAV <stereoworkmail@gmail.com>
2022-02-21 02:03:57 +0300

    fix return errors in LimitTrieMapCache

--- rspace/src/main/scala/coop/rchain/rspace/history/FastLimitTrieMapCache.scala
+++ rspace/src/main/scala/coop/rchain/rspace/history/FastLimitTrieMapCache.scala
@@ -23,26 +23,29 @@ class FastLimitTrieMapCache[A, B](maxSize: Int, cache: TrieMap[A, (B, Option[A],
 
     if (optionValue.isEmpty)
       (None, new FastLimitTrieMapCache(maxSize, cache, Some(key), bottomKey))
+    else {
+      val value = Some(optionValue.get._1)
 
-    val value = Some(optionValue.get._1)
-
-    if (topKey.get == key) {
-      (value, new FastLimitTrieMapCache(maxSize, cache, Some(key), bottomKey))
+      if (topKey.get == key) {
+        (value, new FastLimitTrieMapCache(maxSize, cache, Some(key), bottomKey))
+      }
+      else {
+        if (bottomKey.get == key) {
+          val (lastValue, nextKey, _) = cache(bottomKey.get)
+          cache(bottomKey.get) = (lastValue, nextKey, None)
+          val (firstValue, _, prevKey) = cache(bottomKey.get)
+          cache(bottomKey.get) = (firstValue, Some(key), prevKey)
+          (value, new FastLimitTrieMapCache(maxSize, cache, Some(key), cache(key)._2))
+        }
+        else {
+          val (nextValue, nextKey, _) = cache(cache(key)._2.get)
+          cache(cache(key)._2.get) = (nextValue, nextKey, cache(key)._3)
+          val (prevValue, _, prevKey) = cache(cache(key)._2.get)
+          cache(cache(key)._2.get) = (prevValue, cache(key)._2, prevKey)
+          (value, new FastLimitTrieMapCache(maxSize, cache, Some(key), bottomKey))
+        }
+      }
     }
-
-    if (bottomKey.get == key) {
-      val (lastValue, nextKey, _) = cache(bottomKey.get)
-      cache(bottomKey.get) = (lastValue, nextKey, None)
-      val (firstValue, _, prevKey) = cache(bottomKey.get)
-      cache(bottomKey.get) = (firstValue, Some(key), prevKey)
-      (value, new FastLimitTrieMapCache(maxSize, cache, Some(key), cache(key)._2))
-    }
-
-    val (nextValue, nextKey, _) = cache(cache(key)._2.get)
-    cache(cache(key)._2.get) = (nextValue, nextKey, cache(key)._3)
-    val (prevValue, _, prevKey) = cache(cache(key)._2.get)
-    cache(cache(key)._2.get) = (prevValue, cache(key)._2, prevKey)
-    (value, new FastLimitTrieMapCache(maxSize, cache, Some(key), bottomKey))
   }
 
   def update(key: A, value: B): FastLimitTrieMapCache[A, B] = {
@@ -53,15 +56,17 @@ class FastLimitTrieMapCache[A, B](maxSize: Int, cache: TrieMap[A, (B, Option[A],
       cache(key) = (value, nextKey, prevKey)
       get(key)._2
     }
-
-    cache(key) = (value, None, topKey)
-    if (topKey.isEmpty)
-      new FastLimitTrieMapCache(maxSize, cache, Some(key), Some(key))
-
-    val nextBottomKey = clearOldItems()
-    val (value, _, prevKey) = cache(topKey.get)
-    cache(topKey.get) = (value, Some(key), prevKey)
-    new FastLimitTrieMapCache(maxSize, cache, Some(key), nextBottomKey)
+    else {
+      cache(key) = (value, None, topKey)
+      if (topKey.isEmpty)
+        new FastLimitTrieMapCache(maxSize, cache, Some(key), Some(key))
+      else {
+        val nextBottomKey = clearOldItems()
+        val (value, _, prevKey) = cache(topKey.get)
+        cache(topKey.get) = (value, Some(key), prevKey)
+        new FastLimitTrieMapCache(maxSize, cache, Some(key), nextBottomKey)
+      }
+    }
   }
 
   @tailrec
